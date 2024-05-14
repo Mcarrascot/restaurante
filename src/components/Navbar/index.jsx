841e538bac0d8d58da406416337b15a9acbc1103
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-05-13 22:14:34 -0400

    Navbar añadido

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+function NavBar() {
+  return (
+    <Navbar expand="lg"  data-bs-theme="dark" fixed='top' className='navbar-custom' style={{
+      backgroundColor: '#38000e !important'
+    }}>
+      <Container>
+        <Navbar.Brand href="#home">Los Pollos Hermanos</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className="me-auto" navbarScroll>
+            <Nav.Link href="/#home">Home</Nav.Link>
+            <Nav.Link href="/#about">Nosotros</Nav.Link>
+            <Nav.Link href="/#contáctanos">Contáctanos</Nav.Link>
+            {/* <Nav.Link href="/reservas">Reservas</Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+
+
+export default NavBar;
