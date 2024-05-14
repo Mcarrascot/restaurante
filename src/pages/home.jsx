cfae83e385afd594ee2e843b48f3e7d3090c11a2
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-05-13 22:29:18 -0400

    Home page componente añadido

---
+++ src/pages/home.jsx
@@ -0,0 +1,15 @@
+import SobreNosotrosSeccion from '../components/SobreNosotrosSeccion';
+import ContactSection from '../components/ContactSection';
+import HeroSection from '../components/HeroSection';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <SobreNosotrosSeccion />
+      <ContactSection />
+    </>
+  );
+};
+
+export default Home;
