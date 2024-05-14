83d8e6457df4b0c234f0fd38e722488c6ad4bef8
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-05-13 22:06:24 -0400

    bootstrap-icons añadido

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Import bootstrap css 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
+import { Routes } from './routes';
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NavBar routes={Routes} />
+    <RouterProvider router={Routes} />
+    <Footer />
+  </React.StrictMode>
+);
