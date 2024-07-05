1fbbbd9add959c3d5f1541adc295ac0f8b4836a3
Montserrat Carrasco <mfcarrascot@gmail.com>
2024-07-05 18:07:26 -0400

    fix: arreglé el navbar link

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,9 +11,9 @@ function NavBar() {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto" navbarScroll>
-            <Nav.Link href="/restaurane/#home">Home</Nav.Link>
-            <Nav.Link href="/restaurane/#about">Nosotros</Nav.Link>
-            <Nav.Link href="/restaurane/#contáctanos">Contáctanos</Nav.Link>
+            <Nav.Link href="/restaurante/#home">Home</Nav.Link>
+            <Nav.Link href="/restaurante/#about">Nosotros</Nav.Link>
+            <Nav.Link href="/restaurante/#contáctanos">Contáctanos</Nav.Link>
             {/* <Nav.Link href="/reservas">Reservas</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
